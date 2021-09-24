ad8bc6cfd050d960bea950ca6057f4002bd213ce
nwaegner <nwaegner@diw.de>
2021-09-24 15:47:33 +0200

    Add documentation for animal sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,14 @@
+#' Make animal sounds
+#'
+#' This function creates a sentence for a given animal and sound.
+#'
+#' @param animal Character string that is the name of an animal
+#' @param sound Character string that is the sound of an animal
+#'
+#' @return A character string that is a sentence telling us what sound the given animal makes
+#' @export
+#'
+#' @examples
 animal_sounds <- function(animal, sound) {
   assertthat::assert_that(
     assertthat::is.string(animal),
